e4cc3875f68954320ba3f55b02d0ff7fe493a405
valkim55 <vesper5521@gmail.com>
2022-10-26 11:13:14 -0400

    inquirer logic set

--- index.js
+++ index.js
@@ -3,81 +3,130 @@ const con = require('./db/connection');
 const cTable = require('console.table');
 const inquirer = require('inquirer');
 
-const {getAllDeps, getAllRoles, getAllEmployees} = require('./utils/mysqlQueries')
-
-con.connect(err => {
-    if(err) throw err;
-    //console.log(`connected to assignment12 database!`);
-});
+const {getAllDeps, getAllRoles, getAllEmployees, addDepartment, addRole} = require('./utils/mysqlQueries')
 
 
-
-const actionPrompt = {
-    type: 'list',
-    name: 'action',
-    message: 'Where would you like to start?',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
-};
-
-const nextPrompt = {
-    type: 'list',
-    name: 'action',
-    message: 'What would you like to do next?',
-    choices: ['View all departments', 'View all roles', 'View all employees', 'Add a department', 'Add a role', 'Add an employee', 'Update an employee role']
-}
-
-
-
-function loadHogwartsData() {
-    console.log('Welcome to Hogwarts, Mr. Fudge!');
-    viewAll();
-}
-
-
-function viewAll() {
-    inquirer.prompt(actionPrompt).then((answers) => {
-       
+const firstPrompt = () => {
+    return inquirer.prompt([    
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What would you like to do?',
+            choices: ['View all departments', 'View all roles', 'View all employees', 'Make changes']
+        }   
+    ]).then((answers) => {
+        
         if(answers.action === 'View all departments') {
-            
-            getAllDeps();
-            
-
+           return getAllDeps()
+            .then(() => {
+                return firstPrompt();
+            });
         } else if(answers.action === 'View all roles') {
-            getAllRoles();
-            startOver();
-
+            return getAllRoles()
+            .then(() => {
+                return firstPrompt();
+            });
         } else if(answers.action === 'View all employees') {
-            getAllEmployees();
-            startOver();
+            return getAllEmployees()
+            .then(() => {
+                return firstPrompt();
+            })
         } else {
-            makeChanges();
+            return makeChanges();
         }
-    });
+    
+    })
 };
 
 
 function makeChanges() {
-    inquirer.prompt(actionPrompt).then((answers) => {
-        if(answers.firstAction === 'Add a department') {
+    return inquirer.prompt([    
+        {
+            type: 'list',
+            name: 'action',
+            message: 'What changes would you like to make?',
+            choices: ['Add a department', 'Add a role', 'Add an employee', 'Update an employee role', 'Exit']
+        }   
+    ]).then((answers) => {
+        if(answers.action === 'Add a department') {
+            console.log('adding a new department!')
+            return inquirer.prompt([    
+                {
+                    type: 'input',
+                    name: 'newDep',
+                    message: 'Please enter new department name: ',
+                    validate: newDepInput => {
+                        if(newDepInput) {
+                            addDepartment(newDepInput);                            
+                            return true;
+                        } else {
+                            console.log("You have to provide department name");
+                            return false;
+                        }
+                    } 
+                } 
+            ]).then(() => getAllDeps()).then(() => firstPrompt());
 
-        } else if(answers.firstAction === 'Add a role') {
+        } else if(answers.action === 'Add a role') {
+            console.log('adding a new role!')
+            return inquirer.prompt([
+                {
+                    type: 'input',
+                    name: 'roleTitle',
+                    message: 'Please enter new role title: ',
+                    validate: roleTitleInput => {
+                        if(roleTitleInput) {                           
+                            return true;
+                        } else {
+                            console.log("You have to provide a new title");
+                            return false;
+                        }
+                    } 
+                },
+                {
+                    type: 'input',
+                    name: 'roleSalary',
+                    message: 'What is the salary for this role? ',
+                    validate: roleSalaryInput => {
+                        if(roleSalaryInput) {                           
+                            return true;
+                        } else {
+                            console.log("You have to provide a salary");
+                            return false;
+                        }
+                    } 
+                },
+                {
+                    type: 'input',
+                    name: 'roleDep',
+                    message: 'Which department does this role belong to?',
+                    validate: roleDepInput => {
+                        if(roleDepInput) {                           
+                            return true;
+                        } else {
+                            console.log("You have to provide a name of the corresponding department");
+                            return false;
+                        }
+                    } 
+                }
+            ]).then(({answers}) => addRole({answers})).then(() => firstPrompt());
 
-        } else if(answers.firstAction === 'Add an employee') {
-
-        } else if(answers.firstAction === 'Update an employee role') {
-
-        } else {
+        } else if(answers.action === 'Add an employee') {
+            return console.log('adding a new employee!')
+        } else if(answers.action === 'Update an employee role') {
+            return console.log('updating records!')
+        } else if(answers.action === 'Exit') {
             return;
         }
     })
+    
 }
 
 
-function startOver() {
-    inquirer.prompt(nextPrompt).then((answers) => {
-        viewAll(answers);
 
+
+
+firstPrompt()
+    .then(() => {
+        console.log('hi');
     })
-}
-
-loadHogwartsData();
--- utils/mysqlQueries.js
+++ utils/mysqlQueries.js
@@ -1,29 +1,70 @@
 
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 const con = require('../db/connection');
 const cTable = require('console.table');
 
 
 const getAllDeps = () => {
-    con.query('SELECT * FROM departments', (err, rows) => {
-        if(err) throw err;
-        console.table(`Hogwarts departments:`, rows);
-    });
-}
+    return con.then((con) => {
+        return con.query('SELECT * FROM departments')})
+        .then(([rows]) => {
+            console.table(`Hogwarts departments:`, rows)
+        }).catch((err) => {
+            console.log(`${err} error ocurred`)
+        })
+};
 
 const getAllRoles = () => {
-    con.query('SELECT roles.id, roles.title, roles.salary, departments.department_name AS department FROM roles LEFT JOIN departments ON roles.department_id = departments.id', (err, rows) => {
-        if(err) throw err;
-        console.table(`Currently occupied positions in all departments: `, rows);
-    });
-}
+    return con.then((con) => {
+        return con.query('SELECT roles.id, roles.title, roles.salary, departments.department_name AS department FROM roles LEFT JOIN departments ON roles.department_id = departments.id')})
+        .then(([rows]) => {
+            console.table(`Currently occupied positions in all departments: `, rows);
+        }).catch((err) => { 
+            console.log(`${err} error ocurred`);
+        })
+};
 
 const getAllEmployees = () => {
-    con.query("SELECT e.id, e.first_name, e.last_name, roles.title, roles.salary, departments.department_name AS department, IFNULL(CONCAT(m.first_name, ' ', m.last_name), 'null') AS 'manager' FROM(((employees e LEFT JOIN roles ON e.role_id = roles.id) LEFT JOIN departments ON roles.department_id = departments.id) LEFT JOIN employees m ON e.manager_id = m.id) ORDER BY e.id", (err, rows) => {
-        if(err) throw err;
-        console.table(`Current Hogwarts staff:`, rows);
-    });
+    return con.then((con) => {
+        return con.query("SELECT e.id, e.first_name, e.last_name, roles.title, roles.salary, departments.department_name AS department, IFNULL(CONCAT(m.first_name, ' ', m.last_name), 'null') AS 'manager' FROM(((employees e LEFT JOIN roles ON e.role_id = roles.id) LEFT JOIN departments ON roles.department_id = departments.id) LEFT JOIN employees m ON e.manager_id = m.id) ORDER BY e.id")})
+        .then(([rows]) => {
+            console.table(`Current Hogwarts staff:`, rows);
+        }).catch((err) => {
+            console.log(`${err} error ocurred`)
+        })    
+};
+
+const addDepartment = (newDep) => {
+    return con.then((con) => {
+        return con.query("INSERT INTO departments (department_name) VALUES (?)", newDep)
+        .then(() => {
+            console.table(`${newDep} added to the database!`);
+            
+        }).catch((err) => {
+            console.log(`${err} error ocurred`)
+        })
+    })
+}
+
+const addRole = ({roleTitle, roleSalary, roleDep}) => {
+    return con.then((con) => {
+        // try selecting a role by dep id first and then insert
+        return con.query("INSERT INTO roles (title, salary, department_id) VALUES (?, ?, ?)", roleTitle, roleSalary, roleDep)
+        .then(() => {
+            console.table(`${roleTitle} added to the database!`);
+        }).catch((err) => {
+            console.log(`${err} error ocurred`)
+        })
+    })
+}
+
+const addEmployee = () => {
+
+}
+
+const updateEmployeeRole = () => {
+
 }
 
 
-module.exports = {getAllDeps, getAllRoles, getAllEmployees};
+module.exports = {getAllDeps, getAllRoles, getAllEmployees, addDepartment, addRole};
